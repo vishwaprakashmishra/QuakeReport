dcd0a0f4b99cf5b45db18802703915609591aac4
vishwa prakash mishra <vishwaprakashdec1995@gmail.com>
2017-05-21 16:21:27 +0530

    added proper color gradient

--- app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeAdapter.java
@@ -1,39 +1,44 @@
 package com.example.android.quakereport;
 
 import android.app.Activity;
-;
+import android.graphics.drawable.GradientDrawable;
 import android.icu.text.DecimalFormat;
 import android.icu.text.SimpleDateFormat;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-import org.w3c.dom.Text;
-
 import java.sql.Date;
 import java.util.ArrayList;
 
+import static android.support.v4.content.ContextCompat.getColor;
+import static com.example.android.quakereport.R.id.magnitude;
+
 public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
     private static final String LOCATION_SEPERATOR = " of ";
+
     /**
      * default constructor for EarthquakeAdapter
+     *
      * @param context current context of the Activity
-     * @param quake ArrayList of Earthquake which contain data
+     * @param quake   ArrayList of Earthquake which contain data
      */
-    public EarthquakeAdapter(Activity context, ArrayList<Earthquake> quake){
-        super(context, 0 , quake);
+    public EarthquakeAdapter(Activity context, ArrayList<Earthquake> quake) {
+        super(context, 0, quake);
     }
+
     @NonNull
     @Override
-    public View getView(int position, View convertView, @NonNull ViewGroup parent){
+    public View getView(int position, View convertView, @NonNull ViewGroup parent) {
         // check if the existing view is being reused, otherwise inflate
         View listItemView = convertView;
-        if( listItemView == null){
+        if (listItemView == null) {
             listItemView = LayoutInflater.from(getContext()).inflate(R.layout.list_item
-            , parent, false );
+                    , parent, false);
 
         }
         // get the object located at the
@@ -42,7 +47,17 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
         // we can change the view of the current quake
 
         // Find the TextView with view ID magnitude
-        TextView magnitudeView = (TextView) listItemView.findViewById(R.id.magnitude);
+        TextView magnitudeView = (TextView) listItemView.findViewById(magnitude);
+
+        // Set the proper background color on the magnitude circle.
+        // Fetch the background from the TextView, which is a GradientDrawable
+        GradientDrawable magnitudeCircle = (GradientDrawable) magnitudeView.getBackground();
+
+        // get the appropriate background color based on the current earthquake magnitude
+        int magnitudeColor = getMagnitudeColor(currentQuake.getEarthquakeMagnitude());
+
+        // set the color on the magnitude circle
+        magnitudeCircle.setColor(magnitudeColor);
         // Format the magnitude to show 1 decimal place
         String formattedMagnitude = formatMagnitude(currentQuake.getEarthquakeMagnitude());
         // Display the magnitude of the current earthquake in that TextView
@@ -50,10 +65,10 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
 
 
         // getting the location string ;
-        String locationString , offsetToLocation;
+        String locationString, offsetToLocation;
         String location = currentQuake.getLocation();
         String[] locationArray = new String[2];
-        if( location.contains(LOCATION_SEPERATOR)){
+        if (location.contains(LOCATION_SEPERATOR)) {
             // Split the string into different parts ( as an array of string )
             // based on the " of " text , We expect an array of 2 string
             // the first String will be "5km N " and the second String location
@@ -76,7 +91,7 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
         // set this text on the number TextView
         offsetTextView.setText(offsetToLocation);
         // Find the TextView of location
-        TextView locationTextView = (TextView ) listItemView.findViewById(R.id.location);
+        TextView locationTextView = (TextView) listItemView.findViewById(R.id.location);
         // set location text on the text view
         locationTextView.setText(locationString);
 
@@ -98,12 +113,41 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
         timeView.setText(formattedTime);
 
 
-
         // Return the whole list item layout (containing 2 TextViews and an ImageView)
         // so that it can be shown in the ListView
         return listItemView;
 
     }
+
+    private int getMagnitudeColor(Double earthquakeMagnitude) {
+        int magnitude = ((int) Math.floor(earthquakeMagnitude));
+
+        switch (magnitude) {
+            case 0:
+            case 1:
+                return ContextCompat.getColor(getContext(), R.color.magnitude1);
+            case 2:
+                return ContextCompat.getColor(getContext(), R.color.magnitude2);
+            case 3:
+                return ContextCompat.getColor(getContext(), R.color.magnitude3);
+            case 4:
+                return ContextCompat.getColor(getContext(), R.color.magnitude4);
+            case 5:
+                return ContextCompat.getColor(getContext(), R.color.magnitude5);
+            case 6:
+                return ContextCompat.getColor(getContext(), R.color.magnitude6);
+            case 7:
+                return ContextCompat.getColor(getContext(), R.color.magnitude7);
+            case 8:
+                return ContextCompat.getColor(getContext(), R.color.magnitude8);
+            case 9:
+                return ContextCompat.getColor(getContext(), R.color.magnitude9);
+            default:
+                return ContextCompat.getColor(getContext(), R.color.magnitude10plus);
+
+        }
+    }
+
     /**
      * Return the formatted date string (i.e. "Mar 3, 1984") from a Date object.
      */
@@ -119,6 +163,7 @@ public class EarthquakeAdapter extends ArrayAdapter<Earthquake> {
         SimpleDateFormat timeFormat = new SimpleDateFormat("h:mm a");
         return timeFormat.format(dateObject);
     }
+
     private String formatMagnitude(double magnitude) {
         DecimalFormat magnitudeFormat = new DecimalFormat("0.0");
         return magnitudeFormat.format(magnitude);
